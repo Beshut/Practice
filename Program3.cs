9705d28cdb4510c55580640a43064c333e0b26ae
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-07 18:48:04 +0300

    Update Program3.cs

--- Program3.cs
+++ Program3.cs
@@ -11,6 +11,6 @@ class Program
         var y = int.Parse(Console.ReadLine());
         double x1 = x * 3 / y;
         Console.WriteLine("Округленный с точностью до 4 знаков результат: {0}/{1}={2:N4}", x * 3, y, x1);
-        
+        Console.ReadKey();
     }
 }
