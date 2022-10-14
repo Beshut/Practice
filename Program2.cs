00d9ed648eb2936f89b3638030c5933003b3d016
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-14 15:25:31 +0300

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,13 +1,22 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("a= ");
-        var a = double.Parse(Console.ReadLine());
-        Console.Write("b= ");
-        var b = double.Parse(Console.ReadLine());
-        Console.WriteLine("{0:N2}/{1:N2}={2:N2}", b, a, b / a);
+        Console.Write("Введите первую сторону: ");
+        var a = int.Parse(Console.ReadLine());
+        Console.Write("Введите вторую сторону: ");
+        var b = int.Parse(Console.ReadLine());
+        Console.Write("Введите угол между сторонами: ");
+        var y_g = int.Parse(Console.ReadLine());
+        double y_r = y_g * (Math.PI / 180);
+        double S;
+        if (y_g == 30)
+            S = (0.5) * a * b * (0.5);
+        else
+            S = (0.5) * a * b * Math.Sin(y_r);
+        Console.WriteLine("Площадь треугольника: {0}", S);
         Console.ReadKey();
     }
 }
