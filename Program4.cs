48c7272c55b87a251f556b4fb98042acd4b4cbbd
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-14 15:27:35 +0300

    Update Program4.cs

--- Program4.cs
+++ Program4.cs
@@ -1,23 +1,12 @@
 using System;
-using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("a= ");
-        var a = int.Parse(Console.ReadLine());
-        Console.Write("b= ");
-        var b = int.Parse(Console.ReadLine());
-        Console.Write("c= ");
-        var c = int.Parse(Console.ReadLine());
-        double f1 = a / Math.PI;
-        double f2 = c / Math.E;
-        double F1 = Math.Round(f1, 4, MidpointRounding.AwayFromZero);
-        double F2 = Math.Round(f2, 3, MidpointRounding.AwayFromZero);
-        double pi = Math.Round(Math.PI, 4, MidpointRounding.AwayFromZero);
-        double e = Math.Round(Math.E, 3, MidpointRounding.AwayFromZero);
-        Console.WriteLine("a/PI={0}/{1}={2:N4}   a+b+c={3}+{4}+{5}={6}   c/E={7}/{8}={9}", a, pi, F1, a, b, c, a+b+c, c, e, F2);
+        int x = 1;
+        double y = Math.Sqrt(x * Math.Sin(2 * x) + Math.Pow(Math.E, -2 * x)) * (x + Math.Log(Math.Sqrt(x)));
+        Console.WriteLine("y = {0}", y);
         Console.ReadKey();
     }
 }
