13e37bd3248529fa34cf6a8e47c4d6ce30d3b741
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-14 15:24:58 +0300

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,13 +1,16 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("a= ");
-        var a = double.Parse(Console.ReadLine());
-        Console.Write("b= ");
-        var b = double.Parse(Console.ReadLine());
-        Console.WriteLine("{0:N3}/{1:N3}={2:N3}", a, b, a / b);
+        Console.Write("Введите четырëхзначное число: ");
+        var A = int.Parse(Console.ReadLine());
+        int a = A / 1000;
+        int b = (A / 100) - (a * 10);
+        int c = (A / 10) - (a * 100 + b * 10);
+        int d = A - a * 1000 - b * 100 - c * 10;
+        Console.WriteLine("({0})({1})({2})({3})", b, a, d, c);
         Console.ReadKey();
     }
 }
