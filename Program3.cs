9d01d0fe8f00531d969cef8e294b9f8cad31e3a0
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-14 15:26:02 +0300

    Update Program3.cs

--- Program3.cs
+++ Program3.cs
@@ -1,16 +1,24 @@
 using System;
-using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("x= ");
-        var x = double.Parse(Console.ReadLine());
-        Console.Write("y= ");
-        var y = int.Parse(Console.ReadLine());
-        double x1 = x * 3 / y;
-        Console.WriteLine("Округленный с точностью до 4 знаков результат: {0}/{1}={2:N4}", x * 3, y, x1);
+        int V = 400;
+        double s = 36;
+        double s1 = 18;
+        double S = s / 100;
+        double S1 = s1 / 100;
+        int Vsr = V / 2;
+        double t = S / Vsr;
+        double a = -(Math.Pow(V, 2)) / (2 * S);
+        double V_18 = Math.Sqrt(2 * a * S1 + Math.Pow(V, 2));
+        double S2 = 99 * S / 100;
+        double V_99 = Math.Sqrt(2 * a * S2 + Math.Pow(V, 2));
+        Console.WriteLine("Время движения: {0} с", t);
+        Console.WriteLine("Ускорение: {0} м/с^2", a);
+        Console.WriteLine("Скорость пули на глубине 18 см: {0} м/с", V_18);
+        Console.WriteLine("Скорость пули после прохождения 99% пути: {0} м/с", V_99);
         Console.ReadKey();
     }
 }
