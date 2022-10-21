6fea98fd93d87c7bbc33036b8c413947bceaebb7
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-21 14:26:14 +0300

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -4,13 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Введите четырëхзначное число: ");
-        var A = int.Parse(Console.ReadLine());
-        int a = A / 1000;
-        int b = (A / 100) - (a * 10);
-        int c = (A / 10) - (a * 100 + b * 10);
-        int d = A - a * 1000 - b * 100 - c * 10;
-        Console.WriteLine("({0})({1})({2})({3})", b, a, d, c);
+        Console.WriteLine("Переменная x может принимать такие значения: -1<=x<=2, x=10");
+        Console.Write("Введите x: ");
+        var x = double.Parse(Console.ReadLine());
+        double y;
+        if (-1 <= x && x <= 2)
+            y = 2 * Math.Sqrt(Math.Abs(x + 1));
+        else
+            y = 2 * Math.Tan(Math.Pow(Math.E, x)) - Math.Sin(x);
+        Console.WriteLine("y = {0}", y);
         Console.ReadKey();
     }
 }
