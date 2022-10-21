84dabb6e09a357deeb83e4596a0ab456d3383de1
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-21 14:52:06 +0300

    Update Program3.cs

--- Program3.cs
+++ Program3.cs
@@ -4,21 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        int V = 400;
-        double s = 36;
-        double s1 = 18;
-        double S = s / 100;
-        double S1 = s1 / 100;
-        int Vsr = V / 2;
-        double t = S / Vsr;
-        double a = -(Math.Pow(V, 2)) / (2 * S);
-        double V_18 = Math.Sqrt(2 * a * S1 + Math.Pow(V, 2));
-        double S2 = 99 * S / 100;
-        double V_99 = Math.Sqrt(2 * a * S2 + Math.Pow(V, 2));
-        Console.WriteLine("Время движения: {0} с", t);
-        Console.WriteLine("Ускорение: {0} м/с^2", a);
-        Console.WriteLine("Скорость пули на глубине 18 см: {0} м/с", V_18);
-        Console.WriteLine("Скорость пули после прохождения 99% пути: {0} м/с", V_99);
+        Console.Write("Введите координату x: ");
+        var x = int.Parse(Console.ReadLine());
+        Console.Write("Введите координату y: ");
+        var y = int.Parse(Console.ReadLine());
+        int xm = Math.Abs(x);
+        int ym = Math.Abs(y);
+        if (y == 0 || x == 0)
+        {
+            if (x == 0 && y <= 23 && y >= 0)
+            {
+                Console.WriteLine("На границе");
+                return;
+            }
+
+            else
+                Console.WriteLine("Нет");
+        }
+
+        if (y == ym && x != xm && y <= 23 && x >= -23 && ym>=xm)
+        {
+            if (ym == xm || y == 23)
+                    Console.WriteLine("На границе");
+            else
+                Console.WriteLine("Да");
+        }
+        else
+            Console.WriteLine("Нет");
         Console.ReadKey();
     }
 }
