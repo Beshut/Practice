6d3ac16addfa7d1608c806ac131b94f0e2617dcd
Beshut <105577734+Beshut@users.noreply.github.com>
2022-10-21 14:56:49 +0300

    Update Program4.cs

--- Program4.cs
+++ Program4.cs
@@ -4,9 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 1;
-        double y = Math.Sqrt(x * Math.Sin(2 * x) + Math.Pow(Math.E, -2 * x)) * (x + Math.Log(Math.Sqrt(x)));
-        Console.WriteLine("y = {0}", y);
+     Console.Write("Введите число: ");
+     var a = int.Parse(Console.ReadLine());
+     double del;
+     int j = 0;
+     for (int i = 1; i<a; i++)
+     {
+      del = a%i;
+      if (del == 0)
+       j = j + i;
+     }
+     if (j == a)
+      Console.WriteLine("Совершенное число");
+     else
+      Console.WriteLine("Не совершенное число");
         Console.ReadKey();
     }
 }
